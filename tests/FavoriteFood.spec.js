d33ab8afe26938b7084a97854381144464383108
pacosta613 <pedro.acosta613@gmail.com>
2016-06-13 17:05:08 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should filter correct list of people', function(){
+    var mockedList = [{
+      name: 'Pedro',
+      favoriteFood: 'Ribeye'
+    }, {
+      name: 'Ang',
+      favoriteFood: 'skirt steak'
+    }, {
+      name: 'Brooke',
+      favoriteFood: 'Everything'
+    }];
+
+    var results = $filter('favoriteFood')(mockedList, 'Ribeye');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Pedro');
+  });
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,10 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should remove all the vowels in a string', function(){
+    var results = $filter('removeAllVowels')('abcdefghijklmnopqrstuvwxyz');
+
+    expect(results).toEqual('bcdfghjklmnpqrstvwxyz');
+  });
+
 });
